71035c63e116f1b3a714988da7144668c0f082d3
Andrix1515 <103001206+Andrix1515@users.noreply.github.com>
2024-11-30 01:23:39 -0500

    Add files via upload

---
+++ 222.js
@@ -0,0 +1,9 @@
+function mostrarModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
+}
+
+function cerrarModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
